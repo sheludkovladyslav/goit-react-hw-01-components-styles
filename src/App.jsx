037766dea1f12a0164e3931278b5b9173be726da
sheludkovladyslav <vladsh2103@gmail.com>
2025-04-02 20:20:53 -0400

    зробив статистику та стилі до неї

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Profile } from "./components/Profile";
 import user from "./data/user.json";
+import statistics from "./data/statistics.json";
+import { Statistics } from "./components/Statistics";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Profile user={user} />
+      <Statistics statistics={statistics} />
     </>
   );
 }
