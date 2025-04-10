f09aa63431a9011d5c4cc3bbddf3451d9563d333
sheludkovladyslav <vladsh2103@gmail.com>
2025-04-10 16:21:11 -0400

    пофіксив та налаштував деплой до Github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/goit-react-hw-01-components/",
+  base: "/goit-react-hw-01-components-styles/",
 });
